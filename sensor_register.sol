6691a2b1cb8da5e7ccf20147084cc9f15a192700
Ahsan Manzoor <ahsan.manzoor@oulu.fi>
2018-09-25 06:35:05 +0300

    update

--- sensor_register.sol
+++ sensor_register.sol
@@ -1,32 +1,38 @@
 pragma solidity ^0.4.24;
 
 contract main{
-    	mapping(bytes32 => device) sensorList;
-    	mapping (address => uint256) balances;
-    	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    
+    mapping(bytes32 => device) sensorList;
+    mapping (address => uint256) balances;
+    
+    uint256 total_sensor= 0;
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    	
 	constructor() public {
 		balances[msg.sender] = 10000;
 	}
+	
 	struct device{
 		string name;
-		string sensor;
+		string sensorID;
 		address creator;
-		uint price;
-		address deviceContract;
-		bytes abiDetail;
-		string publicKey;
+		uint256 price;
     	}
+    	
 	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
 		if (balances[msg.sender] < amount) return false;
 		balances[msg.sender] -= amount;
 		balances[receiver] += amount;
-		Transfer(msg.sender, receiver, amount);
+		emit Transfer(msg.sender, receiver, amount);
 		return true;
 	}
+	
 	function getBalance(address addr) public view returns(uint256) {
 		return balances[addr];
 	}
-    	function stringToBytes32(string _str) public constant returns (bytes32){
+	
+    function stringToBytes32(string _str) public constant returns (bytes32){
         	bytes memory tempBytes = bytes(_str);
         	bytes32 convertedBytes;
         	if(0 == tempBytes.length){
@@ -37,28 +43,32 @@ contract main{
         	}
         	return convertedBytes;
     	}
-	function methodRegister(string _name, string _sensors, address _creator, uint _price, address _deviceContract, bytes _detail, string _publicKey) public {
+    
+	function methodRegister(string _name, string _sensors, uint256 _price) public {
 		bytes32 newKey = stringToBytes32(_name);
+		total_sensor = total_sensor + 1;
 		sensorList[newKey].name = _name;
-		sensorList[newKey].sensor = _sensors;
-		sensorList[newKey].creator = _creator;
+		sensorList[newKey].sensorID = _sensors;
+		sensorList[newKey].creator = msg.sender;
 		sensorList[newKey].price = _price;
-		sensorList[newKey].deviceContract = _deviceContract;
-		sensorList[newKey].abiDetail = _detail;
-		sensorList[newKey].publicKey = _publicKey;
     	}
-    	function getPublicKey(string _name) public constant returns (string _publicKey){
+	
+	function getPrice(string _name) public constant returns (uint256){
         	bytes32 key = stringToBytes32(_name);
-        	_publicKey = sensorList[key].publicKey;
+        	uint _price = sensorList[key].price;
+        	return _price;
     	}
-    	function getDeviceContract(string _name) public constant returns (address _deviceContract){
+   
+   function getMac(string _name) public constant returns (string){
         	bytes32 key = stringToBytes32(_name);
-        	_deviceContract = sensorList[key].deviceContract;
+        	string _mac = sensorList[key].sensorID;
+        	return _mac;
     	}
-    	function getContractAbi(string _name) public constant returns (bytes _abi){
-        	bytes32 key = stringToBytes32(_name);
-        	_abi = sensorList[key].abiDetail;
+    	
+    function getSensors() public constant returns (uint256){
+        	return total_sensor;
     	}
+	
 	function requestData(string _name, uint _time) public returns(bool sufficient) {
         	bytes32 key = stringToBytes32(_name);
         	address _receiver = sensorList[key].creator; 
